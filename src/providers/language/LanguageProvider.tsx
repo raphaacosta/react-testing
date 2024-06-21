d76684bf6533f6056c4b4dec31fef36ad88adfe6
Raphael Costa <rafa.costa0@hotmail.com>
2024-06-21 01:19:11 -0300

    first commit

---
+++ src/providers/language/LanguageProvider.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, useState } from "react";
+
+import LanguageContext from "./LanguageContext";
+import en from "./data/en.json";
+import es from "./data/es.json";
+import ptbr from "./data/ptbr.json";
+import { Language } from "./type";
+
+interface Props {
+  language: Language;
+  children: ReactNode;
+}
+
+export function LanguageProvider({ language, children }: Props) {
+  const [currentLanguage, setCurrentLanguage] = useState<Language>(language);
+
+  const changeLanguage = (language: Language) => setCurrentLanguage(language);
+
+  const labelsDictionary: { [key: string]: { [key: string]: string } } = {
+    en,
+    es,
+    ptbr,
+  };
+
+  const getLabel = (labelId: string) => {
+    const label = labelsDictionary[currentLanguage][labelId];
+    if (!label)
+      throw new Error(
+        `LabelID ${labelId} not found in ${currentLanguage}.json`
+      );
+    return label;
+  };
+
+  return (
+    <LanguageContext.Provider
+      value={{ currentLanguage, changeLanguage, getLabel }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+}
