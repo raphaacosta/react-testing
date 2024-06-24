2582e75f83b1a2095de9703310d51028cfb91dfb
Raphael Costa <rafa.costa0@hotmail.com>
2024-06-24 11:12:50 -0300

    test: test TermsAndConditions component

---
+++ tests/components/TermsAndConditions.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
+
+describe('TemrsAndConditions', () => {
+  it('Should render with correct text and initial state', () => {
+    render(<TermsAndConditions />);
+
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Terms & Conditions');
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  it('Should enable the button when the checkbox is checked', async () => {
+    render(< TermsAndConditions />);
+
+    const checkbox = screen.getByRole('checkbox');
+    const user = userEvent.setup();
+    await user.click(checkbox);
+
+    expect(screen.getByRole('button')).toBeEnabled();
+
+  })
+})
