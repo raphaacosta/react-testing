0b0b0eea011ac1e2ecadc0d1e6883a0b874b8144
Raphael Costa <rafa.costa0@hotmail.com>
2024-07-29 15:21:36 -0300

    test: test ExpandableText component

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,10 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
-  return <TermsAndConditions />
+  return <ExpandableText text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt itaque modi voluptas suscipit animi quaerat veritatis, culpa, eaque eveniet praesentium sapiente repellat ea corporis voluptatem nobis magni, sint rem porro distinctio aspernatur corrupti. Nulla, accusamus non voluptates quam illum voluptas placeat. Commodi eius enim, perferendis vero, atque voluptas itaque quis illo saepe sequi quidem maxime soluta quo libero tenetur aliquam dolorum, architecto nihil cupiditate distinctio fugit reiciendis fuga? Dolorem quo modi amet praesentium hic est atque sequi consequatur, minima iusto quos delectus consectetur porro earum unde corrupti deserunt repellat commodi molestiae iste saepe perspiciatis aperiam! Quibusdam at ipsa temporibus quos." />
 };
 
 export default PlaygroundPage;
